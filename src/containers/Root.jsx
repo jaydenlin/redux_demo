95b51d52f5bbf846eaef90d954092768e657f1f3
JaydenLin <j5905653@gmail.com>
2015-09-06 17:32:55 +0800

    first commit

---
+++ src/containers/Root.jsx
@@ -0,0 +1,12 @@
+var React = require('react/addons');
+var Root = React.createClass({
+
+  render: function() {
+    return (
+      <div>Hello World</div>
+
+    );
+  }
+
+});
+module.exports = Root;
---
+++ src/index.jsx
@@ -0,0 +1,4 @@
+var React=require("react/addons");
+var Root=require("./containers/Root.jsx");
+
+React.render(<Root/>,document.getElementById("root"));
