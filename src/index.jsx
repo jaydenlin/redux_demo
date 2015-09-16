531e90f29fdbf80464d32a0e117eafee8ca66b12
JaydenLin <j5905653@gmail.com>
2015-09-17 00:54:38 +0800

    add redux provider as the wrapper

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,9 @@
 var React=require("react/addons");
 var Root=require("./containers/Root.jsx");
+var store=require("./store/todos.js");
+var Provider=require("react-redux").Provider;
 
-React.render(<Root/>,document.getElementById("root"));
+React.render(
+  <Provider store={store}>
+    {function() { return <Root />; }}
+  </Provider>,document.getElementById("root"));
